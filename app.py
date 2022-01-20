7622e064b1ff1f0054e383890db0395d59b0088d
Shrey681 <61517630+Shrey681@users.noreply.github.com>
2022-01-20 10:15:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=5)
 
+
 def db_connection():
     conn = None
     try:
@@ -23,12 +24,12 @@ def log():
 
 @app.route("/login.html", methods=["POST", "GET"])
 def login():
+    global l_uname
     l_uname = request.form["uname"]
     l_pswd = request.form["pswd"]
     conn = db_connection()
     cursor = conn.cursor()
     sql = "SELECT uname, pswd from users WHERE uname = '{un}' AND pswd = '{pw}'".format(un = l_uname, pw = l_pswd)
-    
     rows = cursor.execute(sql)
     rows = rows.fetchall()
     if len(rows) == 1:
@@ -51,7 +52,7 @@ def signup():
         # session["uname"] = uname
         sql = """INSERT INTO users (email, uname, name, pswd)
                  VALUES (?, ?, ?, ?)"""
-        sqltable = "CREATE TABLE '{un}' (item_id integer PRIMARY KEY, item_name text NOT NULL, category text NOT NULL, price FLOAT NOT NULL, date DATE NOT NULL, time TIME NOT NULL)".format(un = uname)
+        sqltable = "CREATE TABLE '{un}' (item_id integer PRIMARY KEY AUTOINCREMENT, category text NOT NULL, price FLOAT NOT NULL)".format(un = uname)
         cursor = cursor.execute(sqltable)         
         cursor = cursor.execute(sql, (email, uname, name, pswd))
         conn.commit()
@@ -65,13 +66,23 @@ def signup():
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/expense.html")
 def expense():
     return render_template("expense.html")
 
-@app.route("/expense.html")
+@app.route("/expense.html", methods=["POST", "GET"])
 def expense_data():
-    return rediret(url_for("expense_history.html"))
+    conn = db_connection()
+    cursor = conn.cursor()
+    if request.method == "POST":
+        category = request.form["category"]
+        price = request.form["price"]
+        sql = "INSERT into '{un}' (category, price) VALUES (?, ?)".format(un =l_uname)
+        cursor = cursor.execute(sql, (category, price))
+        conn.commit()
+    return redirect(url_for("homepage"))
+    
 
 @app.route("/logout")
 def logout():
