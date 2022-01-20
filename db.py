1ddfe3dd8ebe39991a8d7546b9729fe669f268cb
Shrey681 <61517630+Shrey681@users.noreply.github.com>
2022-01-20 10:16:05 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,21 @@ import sqlite3
 conn = sqlite3.connect("spend.sqlite")
 
 cursor = conn.cursor()
-sql_query = """ CREATE TABLE users (
+sql_query1 = """ CREATE TABLE users (
     uid integer PRIMARY KEY AUTOINCREMENT, 
     email text NOT NULL UNIQUE,
     uname text NOT NULL,
     name text NOT NULL,
     pswd text NOT NULL
 )"""
-cursor.execute(sql_query)
+
+sql_query2 = """ CREATE TABLE receipts (
+    rid integer PRIMARY KEY AUTOINCREMENT, 
+    total real NOT NULL,
+    date text NOT NULL,
+    category text NOT NULL,
+    uid integer,
+    FOREIGN KEY(uid) REFERENCES users(uid)
+)"""
+cursor.execute(sql_query1)
+cursor.execute(sql_query2)
